913dc4eed8d0cafd3543c4f9425eeffdd6be907f
Alvin Yanson <yansonalvin29@gmail.com>
2022-03-20 17:16:43 +0800

    Add project files.

---
+++ ModernDesign/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using ModernDesign.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernDesign.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+
+        public HomeViewModel HomeVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { 
+                return _currentView; 
+            }
+            set { 
+                _currentView = value;
+                OnProperyChanged();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            CurrentView = HomeVM;
+        }
+
+    }
+}
